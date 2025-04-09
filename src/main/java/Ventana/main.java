a2fb2788d8ff88c684cc93d53a9abc2a656e1b1b
Vega <diego.vega.3502@eam.edu.co>
2025-04-08 23:03:00 -0500

    commit inicial

---
+++ src/main/java/Ventana/main.java
@@ -0,0 +1,11 @@
+package Ventana;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// hola mundo
+
+	}
+	
+}
---
+++ src/main/java/tiendaPc_3_0/App.java
@@ -0,0 +1,13 @@
+package tiendaPc_3_0;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
